dc1d61d8cb50493198793ffd13633d4b87503d2f
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2018-05-17 03:32:36 +0000

    Mofification des frontend de bien

--- src/AppBundle/Controller/FrontendController.php
+++ src/AppBundle/Controller/FrontendController.php
@@ -16,7 +16,15 @@ class FrontendController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $bien = $em->getRepository('AppBundle:Bien')->findOneBy(array('slug' => $slug));
-        $similaires = $em->getRepository('AppBundle:Bien')->findBy(array('typebien' => $typebien), array('id' => 'DESC'), 0, 4);
+        $similaires = $em->getRepository('AppBundle:Bien')->findBy(array('typebien' => $bien->getTypebien()->getId()), array('slug' => 'DESC'), 4, 0);
+        $typebiens = $em->getRepository('AppBundle:Typebien')
+            ->findBy(array('statut' => 1), array('libelle' => 'ASC'));
+        $zones = $em->getRepository('AppBundle:Zone')
+            ->findBy(array('statut' => 1), array('libelle' => 'ASC'));
+        $services = $em->getRepository('AppBundle:Service')
+            ->findBy(array('statut' => 1), array('libelle' => 'ASC'));
+        $modes = $em->getRepository('AppBundle:Mode')
+            ->findBy(array('statut' => 1), array('libelle' => 'ASC')); //dump($similaires);die();
 
         // Verification du type de bien puis renvoie a la page correspondante a ce type de bien
         if ($bien->getTypebienslug() === 'immeu'){
@@ -25,6 +33,10 @@ class FrontendController extends Controller
             return $this->render("frontend/immeuble.html.twig", [
                 'immeuble' => $immeuble,
                 'similaires'    => $similaires,
+                'typebiens' => $typebiens,
+                'zones' => $zones,
+                'services' => $services,
+                'modes' => $modes,
             ]);
 
         }elseif ($bien->getTypebienslug() === 'appar'){
@@ -33,6 +45,10 @@ class FrontendController extends Controller
             return $this->render("frontend/appartement.html.twig", [
                 'appartement' => $appartement,
                 'similaires'    => $similaires,
+                'typebiens' => $typebiens,
+                'zones' => $zones,
+                'services' => $services,
+                'modes' => $modes,
             ]);
 
         }elseif ($bien->getTypebienslug() === 'villa'){
@@ -41,6 +57,10 @@ class FrontendController extends Controller
             return $this->render("frontend/villa.html.twig", [
                 'villa' => $villa,
                 'similaires'    => $similaires,
+                'typebiens' => $typebiens,
+                'zones' => $zones,
+                'services' => $services,
+                'modes' => $modes,
             ]);
         }else{
             $autrebien = $em->getRepository('AppBundle:Autrebien')->findOneBy(array('bien' => $bien->getId()));
@@ -48,6 +68,10 @@ class FrontendController extends Controller
             return $this->render("frontend/autrebien.html.twig", [
                 'autrebien' => $autrebien,
                 'similaires'    => $similaires,
+                'typebiens' => $typebiens,
+                'zones' => $zones,
+                'services' => $services,
+                'modes' => $modes,
             ]);
 
         }
